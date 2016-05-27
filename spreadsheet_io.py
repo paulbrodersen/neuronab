60dfe58af1f0f4424dc383639605627415d70b5d
Paul Brodersen <paulbrodersen@gmail.com>
2016-05-27 13:11:25 +0100

    Initial commit spreadsheet_io.

---
+++ spreadsheet_io.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas
+
+def read(file_path):
+    return data_frame
+
+def write(data_frame, file_path):
+    return
+
+def apply(data_frame, func, argument_identifiers=None):
+    return return_values
