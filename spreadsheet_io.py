f1d2577c845f28961387e9adf1bacafc597fc674
Paul Brodersen <paulbrodersen@gmail.com>
2016-06-01 13:55:14 +0100

    Added option to skip rows.

--- spreadsheet_io.py
+++ spreadsheet_io.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pandas
 
+"""
+TODO: save out after each row (but actually this might not be a great idea... -> deferred)
+"""
+
 def apply_and_append(filepath_or_dataframe, func, arguments, returns):
     """
     Reads in a spreadsheet / accepts a pandas data frame, loops over
     rows, and interprets columns specified in 'arguments' as arguments
     to a function 'func', and writes out the return values of 'func'
     under the columns in 'returns'.
+
+    The only key word (and thus column header) reserved is 'skip'.
+    Rows for which 'skip' evaluates to True () are not processed.
 
     Arguments:
     ----------
@@ -29,17 +36,25 @@ def apply_and_append(filepath_or_dataframe, func, arguments, returns):
     """
 
     # get data
-    if type(filepath_or_dataframe) == str:
+    if isinstance(filepath_or_dataframe, str):
         df = _read(filepath_or_dataframe)
-    elif type(filepath_or_dataframe) == pandas.DataFrame:
+    elif isinstance(filepath_or_dataframe, pandas.DataFrame):
         df = filepath_or_dataframe
     else:
         raise ValueError("filepath_or_dataframe neither a string nor a pandas.DataFrame object!" + \
                          "\ntype(filepath_or_dataframe) = {}".format(type(filepath_or_dataframe)))
 
+    # trim empty rows
+    df = df.dropna(how='all')
+
     # loop over rows
     total_rows = df.shape[0]
     for ii in range(total_rows):
+        # test whether to skip row
+        if 'skip' in df.columns:
+            if df.loc[ii, 'skip']:
+                if not np.isnan(df.loc[ii, 'skip']): # nans evaluate to True in python...
+                    continue # ... to next row without processing current row
         # create dictionary
         kwargs = _row_to_dict(df, ii, arguments)
         # pass as key word arguments to function
@@ -49,7 +64,7 @@ def apply_and_append(filepath_or_dataframe, func, arguments, returns):
             df.loc[ii, col] = val
 
     # save out / return data frame
-    if type(filepath_or_dataframe) == str:
+    if isinstance(filepath_or_dataframe, (str, unicode)):
         return _write(df, filepath_or_dataframe)
     else:
         return df
@@ -87,7 +102,7 @@ def _row_to_dict(df, row, keys):
     for key in keys:
         value = df.loc[row, key]
         # check for nan
-        if type(value) == float: # need to check that type is float as np.isnan does not accept strings
+        if isinstance(value, float): # need to check that type is float as np.isnan does not accept strings
             if np.isnan(value):
                 continue # i.e. skip key, value pair
         kv.append((key, value))
