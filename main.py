f6b89b0405be30d9282939b3666bb520bb635106
Paul Brodersen <paulbrodersen@gmail.com>
2016-06-10 14:59:00 +0100

    Added automatic detection of OS to automatically set test spreadsheet paths.

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import spreadsheet_io; reload(spreadsheet_io)
 import synapses; reload(synapses)
 import neurites; reload(neurites)
 
+import os
+import os.path
+import warnings
+
+if os.name in ('posix', 'mac'):
+    test_spreadsheet_path = "./test_images/template_linux.xlsx"
+    test_spreadsheet_path = None
+elif os.name in ('nt'):
+    test_spreadsheet_path = "./test_images/template_windows.xlsx"
+else:
+    test_spreadsheet_path = None
+    warning.warn("Could not detect operating system automatically. " + \
+                 "Automatic tests won't run without user input.")
+
+def prompt_for_existing_path(msg):
+    valid = False
+    attempt = 0
+    while not valid:
+        if attempt > 0:
+            print "Path does not exist!"
+        path = raw_input(msg)
+        valid = os.path.exists(path)
+        attempt += 1
+    return path
+
 def count_synapses(spreadsheet_path):
     """
     Script that runs synapses.count using arguments supplied by spreadsheet at path.
@@ -24,7 +49,9 @@ def count_synapses(spreadsheet_path):
     )
     return
 
-def test_count_synapses(spreadsheet_path="./test_images/template_linux.xlsx"):
+def test_count_synapses(spreadsheet_path=test_spreadsheet_path):
+    if spreadsheet_path == None:
+        spreadsheet_path = prompt_for_existing_path("Please provide the full path to the spreadsheet: \n")
     return count_synapses(spreadsheet_path)
 
 def isolate_neurites(spreadsheet_path):
@@ -42,5 +69,7 @@ def isolate_neurites(spreadsheet_path):
     )
     return
 
-def test_isolate_neurites(spreadsheet_path="./test_images/template_linux.xlsx"):
+def test_isolate_neurites(spreadsheet_path=test_spreadsheet_path):
+    if spreadsheet_path == None:
+        spreadsheet_path = prompt_for_existing_path("Please provide the full path to the spreadsheet: \n")
     return isolate_neurites(spreadsheet_path)
